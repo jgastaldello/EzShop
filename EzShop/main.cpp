04a86d81e6d4cddd8ac302c510295312c1e906f3
Josh G <joshuajag@users.noreply.github.com>
2013-10-02 18:47:10 -0700

    Created VS 2013 Project. Updated Ignore file.

---
+++ EzShop/main.cpp
@@ -0,0 +1,16 @@
+/*
+GPL v3 - October 2013
+EzShop: Shopping, Simplified!
+Created by: Joshua Gastaldello, [add your names here]
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Welcome to EzShop!\n\n";
+
+	return 0;
+}
