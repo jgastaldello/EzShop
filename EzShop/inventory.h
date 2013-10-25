8b1288e178ad0fec4f943e7549757eb6a7883c89
arthurchu <arthurchu.van@gmail.com>
2013-10-25 12:12:28 -0700

    added Unit
    Unit is added

--- EzShop/inventory.h
+++ EzShop/inventory.h
@@ -1,21 +1,20 @@
 #pragma once
-
 #include <string>
-
 using namespace std;
-
 class inventory
 {
-	public:
-		inventory(void);
-		inventory(string n, int s);
-		~inventory(void);
-		void setName(string n);
-		void setSize(int s);
-		string getName() const;
-		int getSize() const;
-	private:
-		int size;
-		string name;
+public:
+	inventory(void);
+	inventory(string n , int s);
+	~inventory(void);
+	void setName(string n);
+	void setSize(int s);
+	void setUnit(string u);
+	string getUnit() const;
+	string getName() const;
+	int getSize() const;
+private:
+	int size;
+	string name;
+	string unit;
 };
-
