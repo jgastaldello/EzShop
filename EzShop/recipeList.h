916e1d2f43a2bc9af56abd301e52e9da048b28ce
arthurchu <arthurchu.van@gmail.com>
2013-11-18 17:40:37 -0800

    recipeList has done (with comments)
    Not sure if we need to create a Recipe or not. Right now, not supported

--- EzShop/recipeList.h
+++ EzShop/recipeList.h
@@ -4,21 +4,33 @@
 class recipeList
 {
 public:
+	//constructor
 	recipeList(void);
+	//destructor
 	~recipeList(void);
+	//add a new recipe to the recipe list
 	void addRecipe(string name);
+	//remove a recipe from the list and the file
 	void removeRecipe(string name);
+	//re-order the list in alphabetical order 
 	void sortRecipe();
+	//save the recipeList into recipeList.csv
 	void saveRecipe();
+	//display the all of the recipe
 	void viewRecipe();
+	//display specific recipe
 	void viewRecipe(string name);
+	//load a recipe
 	bool loadRecipe();
+	//search a recipe from the list
 	int searchRecipe(string target) const;
+	//is the recipeList empty
+	bool isEmpty() const;
+	//not supported yet....
+	//... not sure if we need it
+	void createRecipe(string name);
 	
 private:
-	vector<string> recipe;
-	string recipeName;
-
-	
+	//name of the recipes
+	vector<string> recipe;	
 };
-
