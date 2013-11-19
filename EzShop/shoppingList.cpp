fce32dd9ea1d90621702b152c031f6c378403869
arthurchu <arthurchu.van@gmail.com>
2013-11-18 17:18:30 -0800

    ShoppingList is done (I think)

--- EzShop/shoppingList.cpp
+++ EzShop/shoppingList.cpp
@@ -1,11 +1,30 @@
-#include "shoppingList.h"
-
-
-shoppingList::shoppingList(void)
+#pragma once
+#include "itemList.h"
+#include "recipeList.h"
+#include <fstream>
+#include <iostream>
+class shoppingList
 {
-}
+public:
+  //default constructor.... asssume the inventoryList is called inventoryList.csv
+	shoppingList(void);
+	//overloaded constructor for given the name of inventoryList
+	shoppingList(string inventoryList);
+	~shoppingList(void);
+	// compare the list and add to shopping list
+	void compareList(string recipeName);
+	//save the list
+	void saveList(string fileName);
+	//display the list
+	void viewList() const;
+private:
+  //invetory object
+	itemList inventory;	
+	//shopping list object
+	itemList shopList;
+	//the target recipe object
+	itemList targetList;
+	//the recipeList object
+	recipeList recipe;
+};
 
-
-shoppingList::~shoppingList(void)
-{
-}
