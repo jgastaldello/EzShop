a344a2857b461f435b1a6d236f2ece1e509e5487
arthurchu <arthurchu.van@gmail.com>
2013-11-20 18:37:19 -0800

    Update shoppingList.h

--- EzShop/shoppingList.h
+++ EzShop/shoppingList.h
@@ -6,25 +6,27 @@
 class shoppingList
 {
 public:
-  //default constructor.... asssume the inventoryList is called inventoryList.csv
-	shoppingList(void);
-	//overloaded constructor for given the name of inventoryList
-	shoppingList(string inventoryList);
-	~shoppingList(void);
-	// compare the list and add to shopping list
-	void compareList(string recipeName);
-	//save the list
-	void saveList(string fileName);
-	//display the list
-	void viewList() const;
+		//default constructor.... asssume the inventoryList is called inventoryList.csv
+        shoppingList(void);
+        //overloaded constructor for given the name of inventoryList
+        shoppingList(string List);
+        ~shoppingList(void);
+        // compare the list and add to shopping list
+        void compareList(string recipeName);
+        //save the list
+        void saveList(string fileName);
+        //display the list
+        void viewList() const;
 private:
-  //invetory object
-	itemList inventory;	
-	//shopping list object
-	itemList shopList;
-	//the target recipe object
-	itemList targetList;
-	//the recipeList object
-	recipeList recipe;
+		//invetory object
+        itemList inventory;        
+        //shopping list object
+        itemList shopList;
+        //the target recipe object
+        itemList targetList;
+        //the recipeList object
+        recipeList recipe;
+	//name of the inventory List
+	string inventoryList;
 };
 
